60e96f0525f4c854965afb8e15b05078ccd970d7
Josh Tabakov <joshtabakov@MacBook-Air-2.local>
2025-08-01 12:59:39 +0100

    implement navbar section

--- GSAP_world/src/App.jsx
+++ GSAP_world/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Navbar from './components/Navbar.jsx'
+
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <div className="flex-center h-[100vh]">
-            <h1 className="text-3x1 text-indigo-300">Hello</h1>
-        </div>
+        <main>
+            <Navbar />
+        </main>
     )
 }
 export default App
---
+++ GSAP_world/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { navLinks } from '../../constants/index.js';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCameraRetro } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top',
+            }
+        });
+
+        navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+            backgroundColor: '#00000050', 
+            backgroundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power1.inOut'
+            });
+    })
+    
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <FontAwesomeIcon icon={faCameraRetro} size="2x" />
+                    <p>Ana's Pics</p> 
+                    {/* LET ANA CHANGE NAME BASED ON PREFERENCE */}
+                </a>
+
+                <ul>
+                    {navLinks.map((link) => (
+                       <li key={link.id}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                       </li> 
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
